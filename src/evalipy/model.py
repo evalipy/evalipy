850534c7c7101dc183d5cad4499185bdcd992810
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2023-02-01 02:04:25 +0330

    feat: implemented model module.

---
+++ src/evalipy/model.py
@@ -0,0 +1,25 @@
+import pickle
+from joblib import load
+
+
+class Model:
+    def __init__(self, model, model_type = None, path=None) -> None:
+        self.path = path
+        self.model = self.load_model() if model is None else model
+        self.model_type = type(self.model)
+        self.parameters = self.model.get_params()
+        pass
+
+    def __repr__(self) -> str:
+        return (f"path:{str(self.path)} "
+                f"type:{str(self.model_type)} "
+                f"parameters:{str(self.parameters)}"
+                )
+        pass
+
+    def load_model(self) -> object:
+        try:
+            model = pickle.load(self.path)
+            return model
+        except Exception as ex:
+            print(ex)
