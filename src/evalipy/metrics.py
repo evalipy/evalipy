c0e51661b4c784ad448ca2b222ed3adf34974b4c
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2023-02-01 03:15:57 +0330

    feat: implemented metrics module with 6 of the most popular metrics.

--- src/evalipy/metrics.py
+++ src/evalipy/metrics.py
@@ -1,8 +1,36 @@
 import numpy as np
 
-class metrics:
 
-    def __init__():
+class Metrics:
+    EPSILON = 1e-10
+
+    def __init__(self) -> None:
         pass
 
-    
+    @staticmethod
+    def error(y_true: np.ndarray, y_pred: np.ndarray):
+        return y_true - y_pred
+
+    @staticmethod
+    def MSE(y_true, y_pred):
+        return np.mean(np.square(Metrics.error(y_true, y_pred)))
+
+    @staticmethod
+    def MAE(y_true, y_pred):
+        return np.mean(np.abs(Metrics.error(y_true, y_pred)))
+
+    @staticmethod
+    def R2(y_true, y_pred):
+        return np.sum(np.abs(y_true - y_pred)) / (np.sum(np.abs(y_true - np.mean(y_true))) + Metrics.EPSILON)
+
+    @staticmethod
+    def RMSE(y_true, y_pred):
+        return np.sqrt(Metrics.MSE(y_true, y_pred))
+
+    @staticmethod
+    def RRSE(y_true, y_pred):
+        return np.sqrt(np.sum(np.square(y_true - y_pred)) / np.sum(np.square(y_true - np.mean(y_true))))
+
+    @staticmethod
+    def RAE(y_true, y_pred):
+        return np.sum(np.abs(y_true - y_pred)) / (np.sum(np.abs(y_true - np.mean(y_true))) + Metrics.EPSILON)
