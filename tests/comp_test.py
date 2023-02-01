d98ca2d817b0fa45bdb34430f67ce645e22f003b
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2023-02-01 14:02:25 +0330

    test: compare test implemented.

---
+++ tests/comp_test.py
@@ -0,0 +1,23 @@
+from evalipy.src.evalipy import model, comparator, report
+from sklearn import svm
+from sklearn.tree import DecisionTreeRegressor
+from sklearn import datasets
+
+tree = DecisionTreeRegressor()
+clf = svm.SVC()
+X, y = datasets.load_iris(return_X_y=True)
+clf.fit(X, y)
+tree.fit(X, y)
+
+y_pred_1 = clf.predict(X[0:1])
+y_pred_2 = tree.predict(X[0:1])
+model_1 = model.Model(model=clf)
+model_2 = model.Model(model=tree)
+
+print(model_1)
+print(model_2)
+print(report.Report(model=model.Model(clf), actual_data=y, predicted_data=y_pred_1))
+print(report.Report(model=model.Model(tree), actual_data=y, predicted_data=y_pred_2))
+
+comparator = comparator.Comparator(model_1, model_2, y, y_pred_1, y_pred_2)
+print(comparator)
