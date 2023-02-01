9bc8411a980dda5eb0916d4aec73717bea251f30
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2023-02-01 16:30:07 +0330

    test: package importing tests.

---
+++ src/evalipy/__init__.py
@@ -0,0 +1,6 @@
+from .model import Model
+from .comparator import Comparator
+from .metrics import Metrics
+from .report import Report
+
+__version__ = "0.0.1"
---
+++ tests/evalipy_test.py
@@ -0,0 +1,12 @@
+from evalipy.src.evalipy import Model
+from sklearn import svm
+from sklearn.tree import DecisionTreeRegressor
+from sklearn import datasets
+
+tree = DecisionTreeRegressor()
+clf = svm.SVC()
+X, y = datasets.load_iris(return_X_y=True)
+clf.fit(X, y)
+
+m = Model(clf)
+print(m)
