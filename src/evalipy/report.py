e8d59a2cc2b1304e095dbf7dbe9210a2be951676
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2023-02-01 13:26:58 +0330

    fix: importing modules temporary fix.

--- src/evalipy/report.py
+++ src/evalipy/report.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from metrics import Metrics
-from model import Model
+from .metrics import Metrics
+from .model import Model
 
 
 class Report:
@@ -12,11 +12,11 @@ class Report:
         self.predicted_data = predicted_data
 
     def __repr__(self) -> pd.DataFrame:
-        return pd.DataFrame(
+        return pd.DataFrame.from_dict(
             {"Mean Squared Error(MSE)": Metrics.MSE(self.actual_data, self.predicted_data),
              "Mean Absolute Error(MAE)": Metrics.MAE(self.actual_data, self.predicted_data),
              "R-Squared(R2)": Metrics.R2(self.actual_data, self.predicted_data),
              "Root Mean Square Error(RMSE)": Metrics.RMSE(self.actual_data, self.predicted_data),
              "Root Relative Squared Error(RRSE)": Metrics.RRSE(self.actual_data, self.predicted_data),
              "Relative Absolute Error(RAE)": Metrics.RAE(self.actual_data, self.predicted_data),
-             })
+             }, orient='index', columns=['value'])
