d61ebf0ca7271b90f88632d958dca776d453196d
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2023-02-06 02:56:35 +0330

    test: import test updated.

--- tests/imp_test.py
+++ tests/imp_test.py
@@ -1,8 +1,10 @@
-from evalipy.evalipy import model, report
+from evalipy.evalipy import model
 import pickle
 from sklearn import svm
 from sklearn import datasets
+from sklearn.linear_model import LinearRegression
 
+line = LinearRegression()
 
 
 clf = svm.SVC()
@@ -13,7 +15,9 @@ s = pickle.dumps(clf)
 clf2 = pickle.loads(s)
 y_pred = clf2.predict(X[0:1])
 
-model = model.Model(model=clf2)
-report = report.Report(model=clf2, actual_data=y, predicted_data=y_pred)
-print(model)
-print(report)
+model1 = model.Model(model=clf2)
+# report = report.Report(model=clf2, actual_data=y, predicted_data=y_pred)
+print(model1)
+print(model1.raw_type)
+print(model.Model(line).raw_type)
+# print(report)
